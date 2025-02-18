5aa6aa2f5a793f1cd38502d9e309d523ba85a1bc
Nathan <franzxvaldez@gmail.com>
2025-02-18 23:36:38 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Hero from "./components/hero";
+import Header from "./components/header";
+import About from "./components/about";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
